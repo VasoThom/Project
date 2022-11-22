9ab6aef67ba699bc511a00d25b30eeaae4e74944
Vaso <vasothom@hotmail.com>
2022-11-22 12:22:54 +0100

    main

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -3,11 +3,8 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">Vaso , Hüseyin , Ammar</header>
-      <Footer/>
       <header className="App-header"> HI Vaso , Hüseyin , Ammar</header>
-
+      <Footer />
     </div>
   );
 }
