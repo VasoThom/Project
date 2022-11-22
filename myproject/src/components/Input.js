49925374417e5e33530e54898b96679d190d9c56
Vaso <vasothom@hotmail.com>
2022-11-22 20:22:10 +0100

    typewitter

--- myproject/src/components/Input.js
+++ myproject/src/components/Input.js
@@ -1,6 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Input() {
+  const api = {
+    key: "80759a4da43f60267812de0693781be1",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          // console.log(weather);
+          console.log(result);
+        });
+    }
+  };
+
   const data = (d) => {
     let days = [
       "Sunday",
@@ -34,23 +57,56 @@ export default function Input() {
     return `${day} ${date} ${month} ${year}`;
   };
   return (
-    <div>
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 15
+            ? `app warm`
+            : `app`
+          : `main`
+      }
+    >
       <main>
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
             placeholder="search a location:"
+            value={query}
+            onKeyPress={search}
+            onChange={(e) => setQuery(e.target.value)}
           />
         </div>
-        <div className="location-box">
-          <div className="location">Grecce, GR</div>
-          <div className="date">{data(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temperature"> 15 °C </div>
-          <div className="weather"> Sunny day </div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{data(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temperature">
+                {Math.round(weather.main.temp)} °C
+              </div>
+              <div className="weather"> {weather.weather[0].main} </div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h1>
+              <Typewriter
+                words={["Vaso", "Huseyin", "Mohamad Ammar", "!"]}
+                loop={true}
+                cursor
+                cursorStyle="_"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+              />
+            </h1>
+          </>
+        )}
       </main>
     </div>
   );
