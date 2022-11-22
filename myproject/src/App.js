3e315ef84fe91d7d779b89ca72d04ced162589b7
Ammar <mhd.ammar.dachak@hotmail.com>
2022-11-22 10:24:39 +0100

    ammar

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Vaso , Hüseyin</header>
+      <header className="App-header">Vaso , Hüseyin , Ammar</header>
     </div>
   );
 }
