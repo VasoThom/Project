2974d92b26376ce216ddcf37df0e9d36046eed54
Ammar <mhd.ammar.dachak@hotmail.com>
2022-11-22 11:51:53 +0100

    ammar

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
       <header className="App-header">Vaso , Hüseyin , Ammar</header>
+      <Footer/>
+
+      
     </div>
   );
 }
