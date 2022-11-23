ee4cc7805bc9b02764f73c4f18fcaacf45a496bf
Vaso <vasothom@hotmail.com>
2022-11-23 14:35:10 +0100

    vercel

--- myproject/src/components/Footer.js
+++ myproject/src/components/Footer.js
@@ -1,14 +1,17 @@
-
 function Footer() {
-return (
-    
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          Weather App
-        </a>
-      </div>
-
-    );
+  return (
+    <div
+      className="text-center p-3"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+    >
+      &copy; {new Date().getFullYear()} Copyright:{" "}
+      <a
+        className="text-dark"
+        href="https://myproject-ngkyztxs8-vasothom.vercel.app/"
+      >
+        Weather App
+      </a>
+    </div>
+  );
 }
 export default Footer;
