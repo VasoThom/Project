6dd30a0b863df59907d60d81e483153d367124eb
Vaso <vasothom@hotmail.com>
2022-11-26 20:52:00 +0100

    nav a tag

--- myproject/src/components/Navbar.js
+++ myproject/src/components/Navbar.js
@@ -4,9 +4,7 @@ export default function Navbar() {
   return (
     <nav class="navbar bg-warning">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Weather App Project
-        </a>
+        <a class="navbar-brand">Weather App Project</a>
       </div>
     </nav>
   );
